e2073a890697a46fe5b3295b153990b5e48d60ab
Sven Keidel <svenkeidel@gmail.com>
2017-08-01 00:45:46 +0200

    initial commit

---
+++ default.nix
@@ -0,0 +1,15 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  texEnv = pkgs.texlive.combine {
+      inherit (pkgs.texlive) scheme-small latexmk pgf tikz-cd;
+  };
+
+in pkgs.stdenv.mkDerivation {
+  name = "categories";
+  version = "0.0.1";
+  src = ./.;
+  buildInputs = with pkgs; [
+    coq_8_6 texEnv
+  ];
+}
